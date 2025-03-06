8dee7e1153d175445530015d59d014764b1f05c0
Genesis Nahomi Aparicio Acan <3072986550401@ingenieria.usac.edu.gt>
2025-03-06 11:43:49 -0600

    front login

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import AppRouter from "./routes/AppRouter";
+//import  AuthProvider  from './context/AuthContext';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+
+function App() {
+  return (
+    
+      <AppRouter />
+  );
+
+}
+
+export default App;
--- Frontend/src/pages/paciente/index.js
+++
@@ -1,6 +0,0 @@
-export * from './HomePage';
-export * from './RecetasPage';
-export * from './AppointmentsRecordPage';
-export * from './RegisterPacient';
-export * from './EditarPaciente';
-export * from './BuscarExpediente';
