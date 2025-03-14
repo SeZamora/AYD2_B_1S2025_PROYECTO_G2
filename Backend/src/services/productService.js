b1cee176bfb0fabbaa3215d65c1f25c4498c7415
LestherLopez <3010308470101@ingenieria.usac.edu.gt>
2025-03-14 01:36:25 -0600

    202110897: addEmplyee endpoint

--- Backend/src/services/productService.js
+++ Backend/src/services/productService.js
@@ -1,13 +1,30 @@
 const db = require('../services/DBService').default;
+const S3Service = require('../services/S3Service');
 
 const addProduct = async ({ nombre, descripcion, codigo, categoria, precio_compra, precio_venta, cantidad, imagen }) => {
     try {
+        let imageUrl = null;
+
+        // Verificar si recibimos el buffer de la imagen
+        if (imagen && Buffer.isBuffer(imagen)) {
+            const contentType = "image/png";  // O ajusta esto dinámicamente si puedes determinar el tipo de imagen
+
+            // Llamar al método para subir el buffer a S3
+            const [uploadedUrl, uploadError] = await S3Service.uploadBuffer(imagen, contentType, "productos");
+            if (uploadError) {
+                return { success: false, message: 'Error al subir la imagen.' };
+            }
+            imageUrl = uploadedUrl;
+        }
+
+        // Realizar la inserción del producto en la base de datos
         const result = await db.query(
             `INSERT INTO producto (nombre, descripcion, codigo, categoria, precio_compra, precio_venta, cantidad, imagen) 
              VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-            [nombre, descripcion, codigo, categoria, precio_compra, precio_venta, cantidad, imagen]
+            [nombre, descripcion, codigo, categoria, precio_compra, precio_venta, cantidad, imageUrl]
         );
 
+        // Verificar si la inserción fue exitosa
         if (result.affectedRows > 0) {
             return { success: true, message: 'Producto agregado exitosamente.', id_producto: result.insertId };
         } else {
@@ -18,6 +35,7 @@ const addProduct = async ({ nombre, descripcion, codigo, categoria, precio_compr
         return { success: false, message: 'Error interno del servidor.' };
     }
 };
+
 
 
 const getAllProducts = async () => {
--- model/init.sql
+++ model/init.sql
@@ -32,7 +32,7 @@ CREATE TABLE producto (
     precio_compra DECIMAL(10,2) NOT NULL,
     precio_venta  DECIMAL(10,2) NOT NULL,
     cantidad      INT NOT NULL,
-    imagen        MEDIUMBLOB NOT NULL,
+    imagen        VARCHAR(100) NOT NULL,
     PRIMARY KEY (id_producto)
 );
 CREATE TABLE gerente (
@@ -66,7 +66,7 @@ CREATE TABLE empleados (
     edad                     INT NOT NULL,
     genero                   VARCHAR(10) NOT NULL,
     fecha                    DATE NOT NULL,
-    fotografia               MEDIUMBLOB NOT NULL,
+    fotografia               VARCHAR(100) NOT NULL,
     supervisores_id_supervisor INT NOT NULL,
     verificado               INT NOT NULL,
     PRIMARY KEY (empleados_id),
